ad808ec7d46dd47d101c3f99b2a937675024b698
viktoria.zsolnai <viktoria.zsolnai@edu.bme.hu>
2022-10-21 18:52:17 +0200

    routing and mws in progress

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res, next) => {
-  res.send('Hello World!');
-});
+app.use(express.static('static'));
 
-app.listen(3000, () => {
+var server = app.listen(3000, () => {
     console.log('Running on :3000');
 });
+
+require('./route/index')(app); 
